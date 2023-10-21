80adc3266c428aed6254231b5b40a83d1576ec11
Deepesh Patel <deepesh.p@thinkbridge.in>
2023-10-21 21:31:02 +0530

    feat: testing the releae

---
+++ release.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    branches: ['production'],
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/changelog',
+      '@semantic-release/git',
+      '@semantic-release/github',
+    ],
+  };
+  
